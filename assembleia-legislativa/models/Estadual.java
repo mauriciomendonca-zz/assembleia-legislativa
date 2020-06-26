7ea926e73dcc165d6000e71afd005b703ecdad75
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-06-25 21:38:52 -0300

    Criado os metodos de acesso get e set para o atributo uf

--- assembleia-legislativa/models/Estadual.java
+++ assembleia-legislativa/models/Estadual.java
@@ -3,8 +3,16 @@ package models;
 public class Estadual extends Deputado {
     protected String uf;
 
-    public Estadual(String nome, String partido, double salarioBruto, double salarioLiquido, Funcionario[] funcionarios, int numeroCadastro) {
+    public Estadual(String nome, String partido, double salarioBruto, double salarioLiquido, Funcionario[] funcionarios, String uf) {
         super(nome, partido, salarioBruto, salarioLiquido, funcionarios);
+        this.uf = uf;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
         this.uf = uf;
     }
 }
