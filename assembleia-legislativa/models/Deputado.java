70a326e67296fb3bcbaae2b63109c5aba290df71
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-06-25 21:10:58 -0300

    Classe Deputado criada com os seus atributos

---
+++ assembleia-legislativa/models/Deputado.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Deputado {
+    private String nome;
+    private String partido;
+    private double salarioBruto;
+    private double salarioLiquido;
+    private Funcionario[] funcionarios;
+}
