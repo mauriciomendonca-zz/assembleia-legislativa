c28f27725ef9844236489285b82e8f3acbf9ba70
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-06-25 21:16:54 -0300

    Classe Deputado com o metodo construtor

--- assembleia-legislativa/models/Deputado.java
+++ assembleia-legislativa/models/Deputado.java
@@ -6,4 +6,12 @@ public class Deputado {
     private double salarioBruto;
     private double salarioLiquido;
     private Funcionario[] funcionarios;
+
+    public Deputado(String nome, String partido, double salarioBruto, double salarioLiquido, Funcionario[] funcionarios) {
+        this.nome = nome;
+        this.partido = partido;
+        this.salarioBruto = salarioBruto;
+        this.salarioLiquido = salarioLiquido;
+        this.funcionarios = funcionarios;
+    }
 }
