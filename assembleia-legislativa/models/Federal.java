f4b1dc7adfa2377bf891e4ebce61930bd8a065f3
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-06-25 21:34:16 -0300

    Criado o set e get do atributo numeroCadastro

--- assembleia-legislativa/models/Federal.java
+++ assembleia-legislativa/models/Federal.java
@@ -7,4 +7,12 @@ public class Federal extends Deputado {
         super(nome, partido, salarioBruto, salarioLiquido, funcionarios);
         this.numeroCadastro = numeroCadastro;
     }
+
+    public int getNumeroCadastro() {
+        return numeroCadastro;
+    }
+
+    public void setNumeroCadastro(int numeroCadastro) {
+        this.numeroCadastro = numeroCadastro;
+    }
 }
