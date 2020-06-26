90e7e44525b66b25498f7f7e50cab0901c4598c5
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-06-26 01:30:45 -0300

    Criado os objetos de deputados que serao utilizados para teste

--- assembleia-legislativa/App.java
+++ assembleia-legislativa/App.java
@@ -1,6 +1,16 @@
+import models.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Estadual dep1 = new Estadual("Dep1", "PPT", 20.3, 20.3, 12, "RS");
+        Federal dep2 = new Federal("Dep1", "PPS", 25.000, 32.000, 34, 123);
+        Federal dep3 = new Federal("Dep1", "PLN", 10.000, 8.000, 3, 456);
+        Federal dep4 = new Federal("Dep1", "JNF", 13.000, 13.000, 34, 789);
+        Estadual dep5 = new Estadual("Dep1", "PPL", 9.000, 7.200, 21, "RS");
+        Estadual dep6 = new Estadual("Dep1", "PIL", 22.000, 17.600, 10, "SP");
+        Federal dep7 = new Federal("Dep1", "KML", 32.000, 30.000, 10, 321);
+        Federal dep8 = new Federal("Dep1", "WHM", 25.000, 29.000, 4, 654);
+        Estadual dep9 = new Estadual("Dep1", "GRE", 10.000, 8.000, 9, "SC");
+        Estadual dep10 = new Estadual("Dep1", "MIO", 21.000, 16.800, 32, "RJ");
     }
 }
